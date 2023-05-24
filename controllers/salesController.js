0438e6632adbf1ae270a7f1d070b3a31b745c522
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-05-24 22:29:52 +0600

    sales start

---
+++ controllers/salesController.js
@@ -0,0 +1,10 @@
+import OverallStat from "../models/overallstatModel.js";
+
+export const getSales = async (req, res) => {
+  try {
+    const overallStat = OverallStat.find();
+    res.status(200).json(overallStat[0]);
+  } catch (error) {
+    res.status.json({ message: error.message });
+  }
+};
