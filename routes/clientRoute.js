df0d4264018c1d9cf832913c25fb9b5460fad68e
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-05-24 19:49:25 +0600

    geography done

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { getProducts } from "../controllers/clientController.js";
 import {
+  getProducts,
   getCustomers,
   getTransaction,
-} from "../controllers/customerController.js";
+  getGeography,
+} from "../controllers/clientController.js";
+
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransaction);
+router.get("/geography", getGeography);
 
 export default router;
